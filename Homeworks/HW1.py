05c8cc47a7569007fc0010d588995b567ae63cd7
Yaren Zelal TOSU <yarentosu@gmail.com>
2021-03-24 22:59:49 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,23 @@
-#Explain your work
+"""
 
-#Question 1
-for x in range(a):
-	print(a)
+Homework 1
+
+1) How would you define Machine Learning?
+
+2) What are the differences between Supervised and Unsupervised Learning? Specify example 3 algorithms for each of these.
+
+3) What are the test and validation set, and why would you want to use them?
+
+4) What are the main preprocessing steps? Explain them in detail. Why we need to prepare our data?
+
+5) How you can explore and analyse countionus and discrete variables?
+
+6) Analyse the plot given below. (What is the plot and variable type, check the distribution and make comment about how you can preproccess it.)
+
+In [1]:
+Out[1]:
+<matplotlib.axes._subplots.AxesSubplot at 0x2b8ced01320>
+
+
+"""
+
